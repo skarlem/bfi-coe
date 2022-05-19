cda58e6f8560fea0cc1cab55da7a920f1ebd9161
Carl Michael Jandic <carl.michael.jandic@biotech.cloud>
2022-05-19 14:18:07 +0800

    add backticks to shell script

--- cron-job-update.sh
+++ cron-job-update.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 TEST2="curl -X PATCH http://localhost:4000/coe/update"
 echo $TEST2
-RESPONSE=$TEST2
+RESPONSE=`$TEST2`
--- cron-jobs.sh
+++ cron-jobs.sh
@@ -2,6 +2,6 @@
 
 TEST="curl -d '' http://localhost:4000/coe/add"
 echo $TEST
-RESPONSE=$TEST
+RESPONSE=`$TEST`
 
 
